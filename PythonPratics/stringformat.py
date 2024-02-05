e875006a1cfe952990464b919707f9d4682884f2
subbuROY <subrahmanyamb1551@gmail.com>
2024-02-05 14:21:07 +0530

    string functions

---
+++ PythonPratics/stringformat.py
@@ -0,0 +1,8 @@
+dataString = 'Hi This is Subbu'
+dataAge = 27
+print(dataString.capitalize())
+print(dataString.count("u"))
+print(dataString.index("u"))
+print(dataString.isupper())
+print(dataString.upper())
+print(dataString.islower())
