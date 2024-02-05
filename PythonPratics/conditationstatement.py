20f3adb85580d5e4e8cb4cf38ec5ca05243cbfd6
subbuROY <subrahmanyamb1551@gmail.com>
2024-02-05 15:41:41 +0530

    list

---
+++ PythonPratics/conditationstatement.py
@@ -0,0 +1,27 @@
+# Python Conditions and If statements
+val_1 = 25
+val_2 = 26
+
+if val_1 < val_2:
+    print('value2 is big')
+
+val_1 = 28
+val_2 = 26
+if val_1 > val_2:
+    print("value1 is big")
+else:
+    print("value2 is big")
+
+
+val_1=2
+val_2=2
+if val_1 < val_2:
+    print("value1 is big")
+elif val_1 > val_2:
+    print('value2 is big')
+else:
+    print("Both are equal")
+
+
+
+
