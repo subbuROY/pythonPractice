0eec6ce89cb1689c88481979363f1b7c18303ab2
subbuROY <subrahmanyamb1551@gmail.com>
2024-02-05 11:36:47 +0530

    variables

---
+++ PythonPratics/variable.py
@@ -0,0 +1,16 @@
+# creating Variables
+
+variable1 = 25  # this is int variable
+print(variable1)  # printing variable
+
+# types of variables
+
+my_Name =  "Subbu Bassa" #this is string
+my_age = 27
+my_height=5.10 # this is float
+
+#Now we print all this
+print(my_Name)
+print(my_age)
+print(my_height)
+
