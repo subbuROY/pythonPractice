1abb52ab4ae7d0a2012cfeaa80e4a7162de09268
subbuROY <subrahmanyamb1551@gmail.com>
2024-02-05 14:34:54 +0530

    operaters types

---
+++ PythonPratics/operaters.py
@@ -0,0 +1,35 @@
+#operaters types
+
+'''
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+'''
+
+val1=25
+val2=7
+#Arithmetic operators
+print(val1+val2)
+print(val1-val2)
+print(val1*val2)
+print(val1/val2)
+print(val1%val2)
+print(val1**val2)
+print(val1//val2)
+
+
+#Comparison operators
+print(val1 == val2)
+print(val1 != val2)
+print(val1 < val2)
+print(val1 > val2)
+print(val1 >= val2)
+print(val1 <= val2)
+
+
+
+
